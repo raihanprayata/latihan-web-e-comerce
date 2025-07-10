a158b9d542fa6100d3e399ff85961fc6f61280bc
raihanprayata135@gmail.com <raihanprayata135@gmail.com>
2025-07-10 14:50:09 +0700

    perbaikan sedikit dibagian header

--- e-comerce/src/Component/Header/Header.jsx
+++ e-comerce/src/Component/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="header">
       <h3 className="brand">R Store</h3>
-      <p className="keranjang">Keranjang</p>
+      <p className="keranjang"></p>
     </div>
   );
 };
